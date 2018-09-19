bccde381c58e672df2f602fa055b1b28d4b7d534
RayenStrasser-King <rfstrasser-king@mail.csuchico.edu>
2018-09-19 16:16:47 -0700

    stores first and last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Author: Rayen Strasser-King
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
+        std::string first;
+        std::string last;
 
   //get user input
-
+        std::cout<<"Please Enter Your First Name:\n";
+        std::cin>>first;
+        std::cout<<"Please Enter Your Last Name:\n";
+        std::cin>>last;
   //tell fortune
 
   return 0;
