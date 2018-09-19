516e8377172ceb81cb5bc8fb6ad5debc13d1f23c
RayenStrasser-King <rfstrasser-king@mail.csuchico.edu>
2018-09-19 16:52:23 -0700

    Tells Fortune

--- main.cpp
+++ main.cpp
@@ -9,13 +9,16 @@ int main()
   //declare variables
         std::string first;
         std::string last;
-
+        int lucky;
   //get user input
         std::cout<<"Please Enter Your First Name:\n";
         std::cin>>first;
         std::cout<<"Please Enter Your Last Name:\n";
         std::cin>>last;
-  //tell fortune
 
+        cout<<"Weclome,\n" <<first[0]<<"." <<last[0]<<". " << "here is your fortune...\n";
+
+  //tell fortune
+cout<<"Your Lucky Number Is "<<first.length()<<endl;
   return 0;
 }
